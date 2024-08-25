87e53f025a8ed300017bb370567f5cf1004e4e57
seoularpro <sangkyun.hanpro@gmail.com>
2024-08-25 14:06:18 -0700

    Add files via upload

---
+++ pbce-app/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import LandingPage from "./components/templates/LandingPage";
+// import Navbar from "./components/atoms/molecules/Navbar";
+// import { ThemeProvider } from '@mui/material/styles';
+
+export default function Home() {
+  return (
+    <div>
+      {/* <ScrollToTop /> */}
+      <LandingPage/>
+      {/* <Routes />
+      <Footer /> */}
+    </div>
+  );
+}
