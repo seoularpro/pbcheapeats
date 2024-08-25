23bd9c72a1961805113f2b671ccbe7ddaedc32b2
seoularpro <sangkyun.hanpro@gmail.com>
2024-08-24 23:48:04 -0700

    Add files via upload

---
+++ pbce-app/src/app/components/atoms/Hero.js
@@ -0,0 +1,116 @@
+
+import * as React from 'react';
+import { alpha } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+export default function Hero() {
+    function redirectToStripe(event) {
+        event.preventDefault()
+        console.log('redirecting')
+        window.location.href = 'https://buy.stripe.com/test_3cs5nY0mVdbA6Fa289';
+    }
+    return (
+        <Box
+            id="hero"
+            sx={(theme) => ({
+                width: '100%',
+                backgroundImage:
+                    theme.palette.mode === 'light'
+                        ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
+                        : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
+                backgroundSize: '100% 20%',
+                backgroundRepeat: 'no-repeat',
+            })}
+        >
+            <Container
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    pt: { xs: 14, sm: 20 },
+                    pb: { xs: 8, sm: 12 },
+                }}
+            >
+                <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+                    <Typography
+                        className='leaderBoard_left_h1'
+                        variant="h2"
+                        component="h1"
+                    >
+                        Get All PB Food<br /> Delivered
+                    </Typography>
+                    <Typography className='leaderBoard_left_p' component="p">
+                        We deliver throughout PB for just  <br /> a $5 tip.
+                    </Typography>
+                    {/* <Typography
+                        textAlign="center"
+                        color="text.secondary"
+                        sx={{ alignSelf: 'center', width: { sm: '100%', md: '80%' } }}
+                    >
+                        For just a flat $5 fee, get any local PB restaurant food delivered to your door
+                    </Typography> */}
+                    <Stack
+                        direction={{ xs: 'column', sm: 'row' }}
+                        alignSelf="center"
+                        spacing={1}
+                        useFlexGap
+                        sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+                    >
+                        {/* <TextField
+              id="outlined-basic"
+              hiddenLabel
+              size="small"
+              variant="outlined"
+              aria-label="Enter your email address"
+              placeholder="Your email address"
+              inputProps={{
+                autoComplete: 'off',
+                'aria-label': 'Enter your email address',
+              }}
+            /> */}
+                        <Button variant="contained" color="primary" onClick={redirectToStripe}>
+                            Order now
+                        </Button>
+                    </Stack>
+                    {/* <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+            By clicking &quot;Start now&quot; you agree to our&nbsp;
+            <Link href="#" color="primary">
+              Terms & Conditions
+            </Link>
+            .
+          </Typography> */}
+                </Stack>
+                <Box
+                    id="image"
+                    sx={(theme) => ({
+                        mt: { xs: 8, sm: 10 },
+                        alignSelf: 'center',
+                        height: { xs: 200, sm: 700 },
+                        width: '100%',
+                        backgroundImage:
+                            theme.palette.mode === 'light'
+                                ? 'url("/static/images/templates/templates-images/hero-light.png")'
+                                : 'url("/static/images/templates/templates-images/hero-dark.png")',
+                        backgroundSize: 'cover',
+                        borderRadius: '10px',
+                        outline: '1px solid',
+                        outlineColor:
+                            theme.palette.mode === 'light'
+                                ? alpha('#BFCCD9', 0.5)
+                                : alpha('#9CCCFC', 0.1),
+                        boxShadow:
+                            theme.palette.mode === 'light'
+                                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
+                                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
+                    })}
+                />
+            </Container>
+        </Box>
+    );
+}
