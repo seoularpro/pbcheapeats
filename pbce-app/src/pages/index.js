13c57961cb8e0b53cd789f39fec59d9e53d63976
seoularpro <sangkyun.hanpro@gmail.com>
2024-08-25 18:15:13 -0700

    Update index.js

--- pbce-app/src/pages/index.js
+++ pbce-app/src/pages/index.js
@@ -2,14 +2,12 @@ import Image from "next/image";
 import LandingPage from "../app/components/templates/LandingPage";
 // import Navbar from "./components/atoms/molecules/Navbar";
 // import { ThemeProvider } from '@mui/material/styles';
-import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Home() {
   return (
     <div>
       {/* <ScrollToTop /> */}
       <LandingPage/>
-      <GoogleTagManager gtmId="G-CGHR9EN0JV" />
 
       {/* <Routes />
       <Footer /> */}
