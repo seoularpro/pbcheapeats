9fed654bc34bd48860a9b4de03eb9c33d8ee458e
seoularpro <sangkyun.hanpro@gmail.com>
2024-08-27 12:38:02 -0700

    Update index.js

--- pbce-app/src/pages/index.js
+++ pbce-app/src/pages/index.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 import LandingPage from "../app/components/templates/LandingPage";
 // import Navbar from "./components/atoms/molecules/Navbar";
 // import { ThemeProvider } from '@mui/material/styles';
+import GoogleAnalytics from '../app/GoogleAnalytics';
+
 
 export default function Home() {
   return (
     <div>
       {/* <ScrollToTop /> */}
+      <GoogleAnalytics/>
       <LandingPage/>
 
       {/* <Routes />
