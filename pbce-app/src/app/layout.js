8debff0026dc20a704d398284ba5a7c3b819c981
seoularpro <sangkyun.hanpro@gmail.com>
2024-08-27 11:46:45 -0700

    Update layout.js

--- pbce-app/src/app/layout.js
+++ pbce-app/src/app/layout.js
@@ -2,6 +2,8 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 
 export default function RootLayout(props) {
@@ -17,6 +19,7 @@ export default function RootLayout(props) {
         </AppRouterCacheProvider>
       </body>
       <GoogleAnalytics gaId="G-CGHR9EN0JV" />
+      <GoogleTagManager gtmId="G-CGHR9EN0JV" />
     </html>
   );
 }
